a3f6185d906fc0abcdcb5ab75db6d8fc5f07b101
Andreas Steen Hofstad <52hofand@Andreas-Steen-Hofstad.local>
2026-03-05 14:56:26 +0100

    datastore added and accurate knife throw and mobile support

--- src/server/DataStore/init.server.lua
+++ src/server/DataStore/init.server.lua
@@ -1,3 +1,4 @@
+local ProfileManager = require(script:WaitForChild("ProfileManager"))
 local Module = require(script.OnJoined)
 local Module2 = require(script:WaitForChild("KeyBindHandler"))
 local Module3 = require(script:WaitForChild("LeaderboardsHandler"))
