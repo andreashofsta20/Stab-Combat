ca9db20c891871c1e9b751bc7bb2acec151d2b8d
test6 <52hofand@stud.akademiet.no>
2026-09-19 20:48:32 +0200

    a

--- src/server/DataStore/init.server.lua
+++ src/server/DataStore/init.server.lua
@@ -2,3 +2,9 @@ local ProfileManager = require(script:WaitForChild("ProfileManager"))
 local Module = require(script.OnJoined)
 local Module2 = require(script:WaitForChild("KeyBindHandler"))
 local Module3 = require(script:WaitForChild("LeaderboardsHandler"))
+
+for i , v in pairs(script:GetChildren()) do
+    if v:IsA("ModuleScript") then
+        require(v)
+    end
+end
